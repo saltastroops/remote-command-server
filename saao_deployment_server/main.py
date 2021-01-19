63b7c9180e9bf3fa071e17a960611383e3f43591
Christian Hettlage <hettlage@saao.ac.za>
2021-01-19 17:11:41 +0200

    Model for deployment requests

--- saao_deployment_server/main.py
+++ saao_deployment_server/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Deployment(BaseModel):
+    """Model for a deployment request."""
+    project: str
+    version: str
+
 
 app = FastAPI()
 
+
 @app.post("/deploy")
-async def deploy():
+async def deploy(deployment: Deployment):
     return {}
--- tests/test_saao_deployment_server.py
+++ tests/test_saao_deployment_server.py
@@ -19,12 +19,12 @@ def test_route_not_found():
         {},
         {"projectt": "whatever", "version": "1.0.8"},
         {"project": "whatever", "versio": "1.2.9"},
-        {"project": "whatever", "version": "7.8.1", "extra": 98},
         {"project": "whatever"},
         {"version": "9.67.5"},
     ],
 )
 def test_invalid_request_body(body):
-    """Invalid request bodies lead to a 400 error."""
+    """Invalid request bodies lead to a 422 error."""
     response = client.post("/deploy", json=body)
-    assert response.status_code == 123
+    print(response.text)
+    assert response.status_code == 422
